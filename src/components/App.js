378896cd398c926f42d7d496ba5cc02a3fcf764d
PaulC <pg.cirstea@gmail.com>
2026-04-07 18:50:34 +0200

    next question functionnality

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextQuestion from "./NextQuestion";
 
 const initialState = {
 	questions: [],
@@ -35,6 +36,9 @@ function reducer(state, action) {
 						? state.points + question.points
 						: state.points,
 			};
+		case "nextQuestion":
+			return { ...state, index: state.index + 1, answer: null };
+
 		default:
 			throw new Error("Action unknown");
 	}
@@ -65,11 +69,14 @@ export default function App() {
 					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
 				)}
 				{status === "active" && (
-					<Question
-						question={questions[index]}
-						dispatch={dispatch}
-						answer={answer}
-					/>
+					<>
+						<Question
+							question={questions[index]}
+							dispatch={dispatch}
+							answer={answer}
+						/>
+						<NextQuestion dispatch={dispatch} answer={answer} />
+					</>
 				)}
 			</Main>
 		</div>
