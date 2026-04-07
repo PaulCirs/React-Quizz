9ba4492c11645ca305ed9f4abdbbff4e37b1887b
PaulC <pg.cirstea@gmail.com>
2026-04-07 19:23:38 +0200

    adding finish page with highscore feature

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,17 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextQuestion from "./NextQuestion";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
 	questions: [],
 
 	// can be 'loading', 'error', 'ready', 'active', 'finished'
 	status: "loading",
-	index: 0,
+	index: 14,
 	answer: null,
 	points: 0,
+	highscore: 0,
 };
 
 function reducer(state, action) {
@@ -39,17 +41,21 @@ function reducer(state, action) {
 			};
 		case "nextQuestion":
 			return { ...state, index: state.index + 1, answer: null };
-
+		case "finish":
+			return {
+				...state,
+				status: "finished",
+				highscore:
+					state.points > state.highscore ? state.points : state.highscore,
+			};
 		default:
 			throw new Error("Action unknown");
 	}
 }
 
 export default function App() {
-	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-		reducer,
-		initialState,
-	);
+	const [{ questions, status, index, answer, points, highscore }, dispatch] =
+		useReducer(reducer, initialState);
 	const numQuestions = questions.length;
 	const maxPossiblePoints = questions.reduce(
 		(prev, cur) => prev + cur.points,
@@ -87,8 +93,20 @@ export default function App() {
 							dispatch={dispatch}
 							answer={answer}
 						/>
-						<NextQuestion dispatch={dispatch} answer={answer} />
+						<NextQuestion
+							dispatch={dispatch}
+							answer={answer}
+							index={index}
+							numQuestions={numQuestions}
+						/>
 					</>
+				)}
+				{status === "finished" && (
+					<FinishScreen
+						points={points}
+						maxPossiblePoints={maxPossiblePoints}
+						highscore={highscore}
+					/>
 				)}
 			</Main>
 		</div>
